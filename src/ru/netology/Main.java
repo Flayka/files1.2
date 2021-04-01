334206e73a543522cc4dc254c504a5b2008b2b49
Alexandra <rudakalexx@gmail.com>
2021-04-01 16:40:51 +0300

    Add File.separator

--- src/ru/netology/Main.java
+++ src/ru/netology/Main.java
@@ -8,8 +8,8 @@ import java.util.zip.ZipOutputStream;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        String path = "C://Games/savegames/";
-        String zipPath = "C://Games/savegames/zipOutput.zip";
+        String path = File.separator + "Games" + File.separator +"savegames" + File.separator;
+        String zipPath = File.separator + "Games" + File.separator +"savegames" + File.separator + "zipOutput.zip";
 
         GameProgress game1 = new GameProgress(100, 100, 1, 1);
         GameProgress game2 = new GameProgress(90, 150, 5, 17);
@@ -40,7 +40,7 @@ public class Main {
         try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipPath))) {
             for (int i = 0; i < allFiles.size(); i++) {
                 try (FileInputStream fis = new FileInputStream(allFiles.get(i))) {
-                    ZipEntry entry = new ZipEntry(i + "gamePacked.txt");
+                    ZipEntry entry = new ZipEntry(i + 1 + "gamePacked.txt");
                     zos.putNextEntry(entry);
                     byte[] buffer = new byte[fis.available()];
                     fis.read(buffer);
